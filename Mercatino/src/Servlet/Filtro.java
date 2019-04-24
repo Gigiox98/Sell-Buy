d42c4f506d8d26857019e7303ee22da4a48c4940
CFerrara98 <49783581+CFerrara98@users.noreply.github.com>
2019-04-24 11:28:54 +0200

    Add files via upload

--- Mercatino/src/Servlet/Filtro.java
+++ Mercatino/src/Servlet/Filtro.java
@@ -1,4 +1,4 @@
-package Servlet;
+package Controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -17,488 +17,108 @@ import Model.ProdottoDAO;
 /**
  * Servlet implementation class Filtro
  */
-@WebServlet("/Filtro")
+@WebServlet("/Filtro2")
 public class Filtro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Filtro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		String nome=request.getParameter("nome");
-		String stato=request.getParameter("usato");
-		String stato2=request.getParameter("nuovo");
-		String categoria=request.getParameter("categoria");
-		String prezzo=request.getParameter("prezzo");
-		String località=request.getParameter("località");
-		ProdottoDAO p=new ProdottoDAO();
-		try 
-		{
-			ArrayList<Prodotto> list=p.doRetriveByAll();
-			ArrayList<Prodotto> result=new ArrayList<Prodotto>();
-			ArrayList<Prodotto> result2=new ArrayList<Prodotto>();
-			ArrayList<Prodotto> result3=new ArrayList<Prodotto>();
-			ArrayList<Prodotto> result4=new ArrayList<Prodotto>();
-			
-			if(nome==null && stato!=null || stato2!=null && categoria==null && prezzo==null && località==null)
-			{
-				if(stato!=null && stato2==null)
-				result4=p.doRetriveByCond("stato='usato'");
-				if(stato==null && stato2!=null)
-				result4=p.doRetriveByCond("stato='nuovo'");
-				if(stato==null || stato2==null)
-				result4=p.doRetriveByCond("stato='nuovo' or stato='usato'");
-				request.setAttribute("prodotti",result4);
-				RequestDispatcher view=request.getRequestDispatcher("Filtro.jsp");
-				view.forward(request, response);
-			}
-			
-			if(nome!=null || stato!=null || stato2!=null || categoria!=null || prezzo!=null || località!=null)
-			{
-				for(int i=0; i<list.size(); i++)
-				{
-					if(nome!=null)
-					{
-						if(list.get(i).getNome().contains(nome) || list.get(i).getDescrizione().contains(nome))
-						{
-							if(stato!=null && stato2!=null)
-							{
-								if(list.get(i).getStato()=="usato" || list.get(i).getStato()=="nuovo")
-								{
-									if(categoria!=null)
-									{
-										if(list.get(i).getCod_categoria().contains(categoria))
-										{
-											if(prezzo!=null)
-											{
-												double price=Double.parseDouble(prezzo);
-												if(price<=list.get(i).getPrezzo())
-												{
-													if(località!=null)
-													{
-														if(list.get(i).getLocalità().contains(località))
-														{
-															result.add(list.get(i));
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato==null && stato2!=null)
-							{
-								if(list.get(i).getStato()=="nuovo")
-								{
-									if(categoria!=null)
-									{
-										if(list.get(i).getCod_categoria().contains(categoria))
-										{
-											if(prezzo!=null)
-											{
-												double price=Double.parseDouble(prezzo);
-												if(price<=list.get(i).getPrezzo())
-												{
-													if(località!=null)
-													{
-														if(list.get(i).getLocalità().contains(località))
-														{
-															result.add(list.get(i));
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato!=null && stato2==null)
-							{
-								if(list.get(i).getStato()=="usato")
-								{
-									if(categoria!=null)
-									{
-										if(list.get(i).getCod_categoria().contains(categoria))
-										{
-											if(prezzo!=null)
-											{
-												double price=Double.parseDouble(prezzo);
-												if(price<=list.get(i).getPrezzo())
-												{
-													if(località!=null)
-													{
-														if(list.get(i).getLocalità().contains(località))
-														{
-															result.add(list.get(i));
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-						else if(stato==null && stato2==null)
-						{
-								if(categoria!=null)
-								{
-									if(list.get(i).getCod_categoria().contains(categoria))
-									{
-										if(prezzo!=null)
-										{
-											double price=Double.parseDouble(prezzo);
-											if(price<=list.get(i).getPrezzo())
-											{
-												if(località!=null)
-												{
-													if(list.get(i).getLocalità().contains(località))
-													{
-														result.add(list.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-						}
-					}
-				}
-				
-				for(int i=0; i<result.size(); i++)
-				{
-					if(categoria!=null)
-					{
-						if(result.get(i).getCod_categoria().contains(categoria))
-						{
-								if(stato!=null && stato2!=null)
-								{
-									if(result.get(i).getStato()=="usato" || result.get(i).getStato()=="nuovo")
-									{
-										if(result.get(i).getNome().contains(nome) || result.get(i).getDescrizione().contains(nome))
-										{
-											if(prezzo!=null)
-											{
-												double price=Double.parseDouble(prezzo);
-												if(price<=result.get(i).getPrezzo())
-												{
-													if(località!=null)
-													{
-														if(result.get(i).getLocalità().contains(località))
-														{
-															result2.add(result.get(i));
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							else if(stato==null && stato2!=null)
-							{
-								if(result.get(i).getStato()=="nuovo")
-								{
-									if(result.get(i).getNome().contains(nome) || result.get(i).getDescrizione().contains(nome))
-									{
-										if(prezzo!=null)
-										{
-											double price=Double.parseDouble(prezzo);
-											if(price<=result.get(i).getPrezzo())
-											{
-												if(località!=null)
-												{
-													if(result.get(i).getLocalità().contains(località))
-													{
-														result2.add(result.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato!=null && stato2==null)
-							{
-								if(result.get(i).getStato()=="usato")
-								{
-									if(result.get(i).getNome().contains(nome) || result.get(i).getDescrizione().contains(nome))
-									{
-										if(prezzo!=null)
-										{
-											double price=Double.parseDouble(prezzo);
-											if(price<=result.get(i).getPrezzo())
-											{
-												if(località!=null)
-												{
-													if(result.get(i).getLocalità().contains(località))
-													{
-														result2.add(result.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato==null && stato2==null)
-							{
-								if(result.get(i).getNome().contains(nome) || result.get(i).getDescrizione().contains(nome))
-								{
-									if(prezzo!=null)
-									{
-										double price=Double.parseDouble(prezzo);
-										if(price<=result.get(i).getPrezzo())
-										{
-											if(località!=null)
-											{
-												if(result.get(i).getLocalità().contains(località))
-												{
-													result2.add(result.get(i));
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-				for(int i=0; i<result2.size(); i++)
-				{
-					if(prezzo!=null)
-					{
-						double price=Double.parseDouble(prezzo);
-						if(price<=result2.get(i).getPrezzo())
-						{
-								if(stato!=null && stato2!=null)
-								{
-										if(result2.get(i).getStato()=="usato" || result2.get(i).getStato()=="nuovo")
-										{
-											if(result2.get(i).getNome().contains(nome) || result2.get(i).getDescrizione().contains(nome))
-											{
-												if(categoria!=null)
-												{
-													if(result2.get(i).getCod_categoria().contains(categoria))
-													{
-											
-														if(località!=null)
-														{
-															if(result2.get(i).getLocalità().contains(località))
-															{
-																result3.add(result2.get(i));
-															}
-														}
-													}
-												}
-											}
-										}
-								}
-							else if(stato==null && stato2!=null)
-							{
-								if(result2.get(i).getStato()=="nuovo")
-								{
-									if(result2.get(i).getNome().contains(nome) || result2.get(i).getDescrizione().contains(nome))
-									{
-										if(categoria!=null)
-										{
-											if(result2.get(i).getCod_categoria().contains(categoria))
-											{
-									
-												if(località!=null)
-												{
-													if(result2.get(i).getLocalità().contains(località))
-													{
-														result3.add(result2.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato!=null && stato2==null)
-							{
-								if(result2.get(i).getStato()=="usato")
-								{
-									if(result2.get(i).getNome().contains(nome) || result2.get(i).getDescrizione().contains(nome))
-									{
-										if(categoria!=null)
-										{
-											if(result2.get(i).getCod_categoria().contains(categoria))
-											{
-									
-												if(località!=null)
-												{
-													if(result2.get(i).getLocalità().contains(località))
-													{
-														result3.add(result2.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato==null && stato2==null)
-							{
-								if(result2.get(i).getNome().contains(nome) || result2.get(i).getDescrizione().contains(nome))
-								{
-									if(categoria!=null)
-									{
-										if(result2.get(i).getCod_categoria().contains(categoria))
-										{
-								
-											if(località!=null)
-											{
-												if(result2.get(i).getLocalità().contains(località))
-												{
-													result3.add(result2.get(i));
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-				
-				for(int i=0; i<result3.size(); i++)
-				{
-					if(località!=null)
-					{
-						if(result3.get(i).getLocalità().contains(località))
-						{
-							if(stato!=null && stato2!=null)
-							{
-								if(result3.get(i).getStato()=="usato" || result3.get(i).getStato()=="nuovo")
-								{
-									
-									if(result3.get(i).getNome().contains(nome) || result3.get(i).getDescrizione().contains(nome))
-									{
-										if(categoria!=null)
-										{
-											if(result3.get(i).getCod_categoria().contains(categoria))
-											{
-												if(prezzo!=null)
-												{
-													double price=Double.parseDouble(prezzo);
-													if(price<=result3.get(i).getPrezzo())
-													{
-															result4.add(result3.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato==null && stato2!=null)
-							{
-								if(result3.get(i).getStato()=="nuovo")
-								{
-									if(result3.get(i).getNome().contains(nome) || result3.get(i).getDescrizione().contains(nome))
-									{
-										if(categoria!=null)
-										{
-											if(result3.get(i).getCod_categoria().contains(categoria))
-											{
-												if(prezzo!=null)
-												{
-													double price=Double.parseDouble(prezzo);
-													if(price<=result3.get(i).getPrezzo())
-													{
-															result4.add(result3.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato!=null && stato2==null)
-							{
-								if(result3.get(i).getStato()=="usato")
-								{
+	public Filtro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-									if(result3.get(i).getNome().contains(nome) || result3.get(i).getDescrizione().contains(nome))
-									{
-										if(categoria!=null)
-										{
-											if(result3.get(i).getCod_categoria().contains(categoria))
-											{
-												if(prezzo!=null)
-												{
-													double price=Double.parseDouble(prezzo);
-													if(price<=result3.get(i).getPrezzo())
-													{
-															result4.add(result3.get(i));
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							else if(stato==null && stato2==null)
-							{
-
-								if(result3.get(i).getNome().contains(nome) || result3.get(i).getDescrizione().contains(nome))
-								{
-									if(categoria!=null)
-									{
-										if(result3.get(i).getCod_categoria().contains(categoria))
-										{
-											if(prezzo!=null)
-											{
-												double price=Double.parseDouble(prezzo);
-												if(price<=result3.get(i).getPrezzo())
-												{
-														result4.add(result3.get(i));
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-				
-				request.setAttribute("prodotti",result4);
-				RequestDispatcher view=request.getRequestDispatcher("Filtro.jsp");
-				view.forward(request, response);
-			}
-			
-			else 
-			{
-				request.setAttribute("prodotti",list);
-				RequestDispatcher view=request.getRequestDispatcher("Filtro.jsp");
-				view.forward(request, response);
-			}
-		} 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String stato = request.getParameter("usato");
+		String stato2 = request.getParameter("nuovo");
+		String categoria = request.getParameter("categoria");
+		Double prezzo;
+		try {
+			prezzo = Double.parseDouble(request.getParameter("prezzo"));
+		}
+		catch (Exception e){
+			prezzo = null;
+		}
+		String località = request.getParameter("località");
+		ProdottoDAO p = new ProdottoDAO();
 		
-		catch (SQLException e) 
-		{
+		try {
+			
+			ArrayList<Prodotto> result = new ArrayList<Prodotto>();
+
+			if (nome == null && categoria == null && prezzo == null && località == null) {
+				if (stato != null && stato2 == null)
+					result = p.doRetriveByCond("stato='usato'");
+				else if (stato == null && stato2 != null)
+					result = p.doRetriveByCond("stato='nuovo'");
+				else if (stato == null || stato2 == null)
+					result = p.doRetriveByCond("stato='nuovo' or stato='usato'");
+				else result = null;
+			} else {
+				
+				String query="";
+				
+				if(nome != null && nome != "") {
+					System.out.println("nome:"+nome);
+					query = query + "(nome LIKE '%"+nome+"%' OR descrizione LIKE '%"+nome+"%') ";
+					
+				} 
+				
+				
+				if(categoria != null && categoria != "") {
+					if(!query.equals("")) query = query + "AND ";
+					query = query + "categoria = '"+ categoria + "' ";
+				}
+				
+				if(prezzo != null) {
+					System.out.println(prezzo);
+					if(!query.equals("")) query = query + "AND ";
+					query = query + "prezzo <= "+ prezzo + " ";
+				}
+				
+				if(località != null && località != "") {
+					if(!query.equals("")) query = query + "AND ";
+					query = query + "località LIKE '%"+ località + "%' ";
+				}
+				
+				if((stato!=null) && (stato2 != null)){
+					if(!query.equals("")) query = query + "AND ";
+					query = query + "(stato = 'usato' OR stato = 'nuovo')";
+				} else if(stato != null){
+					if(!query.equals("")) query = query + "AND ";
+					query = query + "stato = 'usato'";
+				} else if(stato2 != null){
+					if(!query.equals("")) query = query + "AND ";
+					query = query + "stato = 'nuovo'";
+				}
+				System.out.println(query);
+				result = p.doRetriveByCond(query);
+			}
+			
+			request.setAttribute("prodotti", result);
+			RequestDispatcher view = request.getRequestDispatcher("Filtro.jsp");
+			view.forward(request, response);
+		}
+
+		catch  (SQLException e){
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
