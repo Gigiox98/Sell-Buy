125935716d5366db661e4361546fd4b501f63dc0
CFerrara98 <49783581+CFerrara98@users.noreply.github.com>
2019-05-01 10:30:30 +0200

    Add files via upload

--- Mercatino/src/Model/ProdottoDAO.java
+++ Mercatino/src/Model/ProdottoDAO.java
@@ -8,11 +8,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-public class ProdottoDAO 
-{
-	final String DB_URL_with_SSL = "jdbc:mysql://localhost:3306/sellbuy?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+public class ProdottoDAO {
+	final String DB_URL_with_SSL = "jdbc:mysql://localhost:3306/sellbuy?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	final String USER = "root";
-	final String PASS = "Rompicapo98";
+	final String PASS = "pippo";
 
 	public int doSave(Prodotto o) throws SQLException, IOException {
 		try {
@@ -30,7 +29,7 @@ public class ProdottoDAO
 				String query = "select cod_p from prodotto where cod_p like '" + o.getCodice() + "';";
 				ResultSet rs = stmt.executeQuery(query);
 				if (!rs.next()) {
-					String query2 = "insert into prodotto(cod_p,nome,quantità,prezzo,descrizione,località,data_inserimento,stato,codice_venditore,nome_categoria,immagine) values ('"
+					String query2 = "insert into prodotto(cod_p,nome,quantità,prezzo,descrizione,località,data_inserimento,stato,codice_venditore,categoria,immagine) values ('"
 							+ o.getCodice() + "','" + o.getNome() + "','" + o.getQuantità() + "','" + o.getPrezzo()
 							+ "','" + o.getDescrizione() + "','" + o.getLocalità() + "','" + o.getData_ins() + "','"
 							+ o.getStato() + "','" + o.getCod_venditore() + "','" + o.getCod_categoria() + "','"
@@ -43,40 +42,44 @@ public class ProdottoDAO
 		}
 	}
 
-	public void doSaveOrUpdate(Prodotto o) throws SQLException {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		}
+	public int doSaveOrUpdate(Prodotto o) throws SQLException {
+	    try {
+	      Class.forName("com.mysql.cj.jdbc.Driver");
+	    }
 
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
+	    catch (ClassNotFoundException e) {
+	      e.printStackTrace();
 
-		}
+	    }
 
-		try (Connection conn = DriverManager.getConnection(DB_URL_with_SSL, USER, PASS)) {
-			try (Statement stmt = conn.createStatement()) {
-				String query = "select cod_p from prodotto where cod_p like '" + o.getCodice() + "';";
-				ResultSet rs = stmt.executeQuery(query);
-				if (!rs.next()) {
-					String query2 = "insert into prodotto(cod_p,nome,quantità,prezzo,descrizione,località,data_inserimento,stato,codice_venditore,nome_categoria,immagine) values ('"
-							+ o.getCodice() + "','" + o.getNome() + "','" + o.getQuantità() + "','" + o.getPrezzo()
-							+ "','" + o.getDescrizione() + "','" + o.getLocalità() + "','" + o.getData_ins() + "','"
-							+ o.getStato() + "','" + o.getCod_venditore() + "','" + o.getCod_categoria() + "','"
-							+ o.getImmagine() + "'" + ");";
-					stmt.executeUpdate(query2);
-				}
+	    try (Connection conn = DriverManager.getConnection(DB_URL_with_SSL, USER, PASS)) {
+	      try (Statement stmt = conn.createStatement()) {
+	    	int x;
+	        String query = "select cod_p from prodotto where cod_p like '" + o.getCodice() + "';";
+	        ResultSet rs = stmt.executeQuery(query);
+	        if (!rs.next()) {
+	          String query2 = "insert into prodotto(cod_p,nome,quantità,prezzo,descrizione,località,data_inserimento,stato,codice_venditore,categoria,immagine) values ('"
+	              + o.getCodice() + "','" + o.getNome() + "','" + o.getQuantità() + "','" + o.getPrezzo()
+	              + "','" + o.getDescrizione() + "','" + o.getLocalità() + "','" + o.getData_ins() + "','"
+	              + o.getStato() + "','" + o.getCod_venditore() + "','" + o.getCod_categoria() + "','"
+	              + o.getImmagine() + "'" + ");";
+	         x =  stmt.executeUpdate(query2);
+	          return x;
+	        } else {
+	          String query2 = "update prodotto set quantità='" + o.getQuantità() + "', nome = '" + o.getNome()
+	              + "', prezzo='" + o.getPrezzo() + "', descrizione='" + o.getDescrizione() + "', località='"
+	              + o.getLocalità() + "', data_inserimento='" + o.getData_ins() + "', stato='" + o.getStato()
+	              + "', codice_venditore='" + o.getCod_venditore() + "' , categoria='" + o.getCod_categoria()
+	              + "' , immagine='"+o.getImmagine()+"'  where cod_p='" + o.getCodice() + "';";
+	         x = stmt.executeUpdate(query2);
+	         return x;
+	        }
+	        
+	      }
+	    }
+	  }
 
-				else {
-					String query2 = "update prodotto set quantità='" + o.getQuantità() + "', nome = '" + o.getNome()
-							+ "', prezzo='" + o.getPrezzo() + "', descrizione='" + o.getDescrizione() + "', località='"
-							+ o.getLocalità() + "', data_inserimento='" + o.getData_ins() + "', stato='" + o.getStato()
-							+ "', codice_venditore='" + o.getCod_venditore() + "' , nome_categoria='" + o.getCod_categoria()
-							+ "' , immagine='"+o.getImmagine()+"' where cod_p='" + o.getCodice() + "';";
-					stmt.executeUpdate(query2);
-				}
-			}
-		}
-	}
+	  
 
 	public void doDelete(Prodotto o) throws SQLException {
 		try {
@@ -90,14 +93,14 @@ public class ProdottoDAO
 
 		try (Connection conn = DriverManager.getConnection(DB_URL_with_SSL, USER, PASS)) {
 			try (Statement stmt = conn.createStatement()) {
-				String query = "delete from prodotto where cod_p like '" + o.getCodice() + "';";
-				stmt.executeQuery(query);
+				System.out.println("code to delete = " + o.getCodice());
+				String query = "delete from prodotto where cod_p = '" + o.getCodice() + "';";
+				stmt.executeUpdate(query);
 			}
 		}
 	}
 
-	public Prodotto doRetriveByKey(String key) throws SQLException, IOException 
-	{
+	public Prodotto doRetriveByKey(String key) throws SQLException, IOException {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -123,14 +126,16 @@ public class ProdottoDAO
 					String data_ins = rs.getString("data_inserimento");
 					String stato = rs.getString("stato");
 					String codice_venditore = rs.getString("codice_venditore");
-					String codice_categoria = rs.getString("nome_categoria");
+					String codice_categoria = rs.getString("categoria");
 					String x = rs.getString("immagine");
-					return new Prodotto(nome, key, quantità, prezzo, descrizione, località, data_ins, stato,
+					System.out.println(q);
+					return new Prodotto(key, nome, quantità, prezzo, descrizione, località, data_ins, stato,
 							codice_venditore, codice_categoria, x);
 				}
 
 				else
-					return null;
+					System.out.println("sono qui");
+				return null;
 			}
 
 			catch (SQLException e) {
@@ -139,8 +144,7 @@ public class ProdottoDAO
 			}
 		}
 
-	
-}
+	}
 
 	public ArrayList<Prodotto> doRetriveByCond(String cond) throws SQLException {
 		try {
@@ -165,9 +169,9 @@ public class ProdottoDAO
 					String data_ins = rs.getString("data_inserimento");
 					String stato = rs.getString("stato");
 					String codice_venditore = rs.getString("codice_venditore");
-					String codice_categoria = rs.getString("nome_categoria");
+					String codice_categoria = rs.getString("categoria");
 					String x = rs.getString("immagine");
-					list.add(new Prodotto(nome, codice, quantità, prezzo, descrizione, località, data_ins, stato,
+					list.add(new Prodotto(codice, nome, quantità, prezzo, descrizione, località, data_ins, stato,
 							codice_venditore, codice_categoria, x));
 				}
 
@@ -179,53 +183,42 @@ public class ProdottoDAO
 		}
 	}
 
- public ArrayList<Prodotto> doRetriveByAll() throws SQLException
- { 
-	 try 
-	 {
-		 Class.forName("com.mysql.cj.jdbc.Driver"); 
-	 } 
-	 catch (ClassNotFoundException e)
-	 { 
-		 e.printStackTrace(); 
-	 }
-	 
-	 try 
-	 (Connection conn =DriverManager.getConnection(DB_URL_with_SSL, USER, PASS)) 
-	 { 
-		 try (Statement stmt = conn.createStatement()) 
-		 { 
-			 String query = "select * from prodotto";
-			 ResultSet rs = stmt.executeQuery(query); 
-			 ArrayList <Prodotto> list=new ArrayList<Prodotto>(); 
-			 while (rs.next()) 
-			 { 
-				 String nome=rs.getString("nome");
-				 String q=rs.getString("quantità");
-				 int quantità=Integer.parseInt(q); 
-				 String p=rs.getString("prezzo"); 
-				 double prezzo=Double.parseDouble(p); 
-				 String descrizione=rs.getString("descrizione");
-				 String località=rs.getString("località"); 
-				 String data_ins=rs.getString("data_inserimento"); 
-				 String stato=rs.getString("stato"); 
-				 String codice_venditore=rs.getString("codice_venditore"); 
-				 String codice_categoria=rs.getString("nome_categoria"); 
-				 String codice=rs.getString("cod_p"); 
-				 String immagine=rs.getString("immagine");
-				 list.add(new Prodotto(nome,codice,quantità,prezzo,descrizione,località,data_ins,stato,codice_venditore,codice_categoria,immagine)); 
+	public ArrayList<Prodotto> doRetriveByAll() throws SQLException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try (Connection conn = DriverManager.getConnection(DB_URL_with_SSL, USER, PASS)) {
+
+			try (Statement stmt = conn.createStatement()) {
+
+				String query = "select * from prodotto";
+				ResultSet rs = stmt.executeQuery(query);
+				ArrayList<Prodotto> list = new ArrayList<Prodotto>();
+				while (rs.next()) {
+					String codice = rs.getString("cod_p");
+					String q = rs.getString("quantità");
+					String nome = rs.getString("nome");
+					int quantità = Integer.parseInt(q);
+					String p = rs.getString("prezzo");
+					double prezzo = Double.parseDouble(p);
+					String descrizione = rs.getString("descrizione");
+					String località = rs.getString("località");
+					String data_ins = rs.getString("data_inserimento");
+					String stato = rs.getString("stato");
+					String codice_venditore = rs.getString("codice_venditore");
+					String codice_categoria = rs.getString("categoria");
+					String x = rs.getString("immagine");
+					list.add(new Prodotto(codice, nome, quantità, prezzo, descrizione, località, data_ins, stato,
+							codice_venditore, codice_categoria, x));
+				}
+
+				return list;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
 			}
-  
-		return list; 
-		 } 
-		 catch (SQLException e) 
-		 { 
-			 e.printStackTrace(); return null;
-		 }
-	} 
+		}
+	}
 }
- 
- 
-}
-
-
