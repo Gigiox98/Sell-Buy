e65cb80b23e9148cd3e3cc31fa5ac7adc73a66fa
CFerrara98 <49783581+CFerrara98@users.noreply.github.com>
2019-05-05 09:42:34 +0200

    Add files via upload

--- Mercatino/Database_Sell&Buy.sql
+++ Mercatino/Database_Sell&Buy.sql
@@ -1,7 +1,8 @@
 create database SellBuy;
 
-use SellBuy;
-
+use sellbuy;
+select * from prodotto where (nome LIKE '%%' OR descrizione LIKE '%%') AND categoria = 'Altro...' AND prezzo <= 0 AND stato = 'usato';
+select * from utente;
 create table utente(
 	username varchar(30) primary key,
     nome varchar(30) not null,
@@ -13,12 +14,9 @@ create table utente(
     email varchar(30),
     adminflag bool not null
 );
-select * from utente;
-delete from utente;
 
 
-
-
+;
 create table categoria(
     categoria varchar(30) primary key
 );
@@ -38,7 +36,8 @@ create table prodotto(
     località varchar(30) not null,
     data_inserimento varchar(50)not null,
 	stato varchar(30) not null,
-    percorso varchar(100),
+    pathImmagine varchar(500) not null,
+    immagine longtext,
     codice_venditore varchar(30),
     categoria varchar(30),
 	FOREIGN KEY (codice_venditore) REFERENCES utente (username)
@@ -47,7 +46,12 @@ create table prodotto(
 	on update cascade on delete cascade
 );
 
-alter table prodotto add column data_inserimento varchar(50)not null;
+delete from prodotto where cod_p = '7979144592';
+update prodotto set quantità = '4', nome = 'Smerigliatrice Angolare', prezzo = '300', 
+descrizione = 'smerigliatrice angolare ultima tecnologia', data_inserimento = 'Sun Apr 21 12:29:33 CEST 2019', 
+stato = 'Nuovo', località = 'Roma', codice_venditore = 'Felix88', categoria = 'Ferramenta' where cod_p='1452351942';
+
+select cod_p from prodotto where cod_p like '1452351942';
 create table ordine(
 	codice_ordine varchar (30)  primary key key,
     indirizzo_spedizione varchar(30) not null,
@@ -62,6 +66,10 @@ create table ordine(
     FOREIGN KEY(username_a) REFERENCES utente(username)
     on update cascade on delete cascade
 );
+select * from ordine where username_a = 'MikeTony19' AND stato = 'in carrello';
+select * from ordine where username_a = 'MikeTony19' AND stato = 'in carrello';
+select * from ordine where username_a = 'MikeTony19' AND stato = 'in carrello';
+select * from ordine;
 
 
 
@@ -72,7 +80,19 @@ FOREIGN KEY(codice_p) REFERENCES prodotto(cod_p)
 on update cascade on delete cascade,
 primary key(sconto, codice_p)
 );
+delete from recensioni;
+create table recensioni(
+codice varchar(15) primary key,
+codice_p varchar(30),
+FOREIGN KEY(codice_p) REFERENCES prodotto(cod_p)
+on update cascade on delete cascade,
+utente varchar(30),
+FOREIGN KEY(utente) REFERENCES utente(username),
+testo longtext,
+voto int
+);
 
+select * from recensioni;
 update categoria  set ricavo_tot = (select sum(costo) 
 from iscrizione, studente
 where iscrizione.cod_studente = studente.codice 
