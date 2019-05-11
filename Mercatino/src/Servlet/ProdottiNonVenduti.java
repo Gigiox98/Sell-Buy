e6df1f23a3bd5af16ca5d16697abbf6fa1827598
Gigiox98 <49783187+Gigiox98@users.noreply.github.com>
2019-05-11 22:11:28 +0200

    Add files via upload

---
+++ Mercatino/src/Servlet/ProdottiNonVenduti.java
@@ -0,0 +1,61 @@
+package Servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Model.Prodotto;
+import Model.ProdottoDAO;
+
+/**
+ * Servlet implementation class ProdottiNonVenduti
+ */
+@WebServlet("/ProdottiNonVenduti")
+public class ProdottiNonVenduti extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProdottiNonVenduti() 
+    {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		ProdottoDAO p=new ProdottoDAO();
+		ArrayList <Prodotto> list= new ArrayList<Prodotto>();
+		try 
+		{
+			list=p.doRetriveByCond(" acquistato<=3");
+			request.setAttribute("prodotti", list);
+			RequestDispatcher view=request.getRequestDispatcher("/ProdottiPocoVenduti.jsp");
+			view.forward(request, response);
+		} 
+		
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		doGet(request, response);
+	}
+
+}
